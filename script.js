ddf68eeb8981bd1cc6ba6983c0910d2f69badabd
Pasha127 <pasha.account@gmail.com>
2022-07-30 23:31:24 +0200

    starting to get memory to work

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 //document.location.reload(true); // debug refresh
 //javascript:window.close('','_parent',''); //debug closer
 //.style.backgroundColor = "#000000";  //debug colorer
+
+//declaration of DOM object variables
+
 const body = document.querySelector("body");
 const nameInput = document.querySelector(".nameInput");
 const tierInput = document.querySelector(".tierInput")
 const enterPlayerButton = document.querySelector(".enterPlayerButton");
+const resetPlayerButton = document.querySelector(".resetPlayerButton");
 const menuButton = document.querySelector(".menuButton");
 const mainMenuBody = document.querySelector(".mainMenuBody");
 const searchIcon = document.querySelector(".searchIcon");
+const participantZone = document.querySelector(".participantZone");
 const poolParticipantContainer = document.querySelector(".poolParticipantContainer");
 let poolParticipants = document.querySelectorAll(".poolParticipant");
 let poolAddToTeamButtons = document.querySelectorAll(".poolAddToTeamButton");
@@ -38,15 +43,68 @@ const menuLoad = document.querySelector(".menuLoad")
 const menuPrint = document.querySelector(".menuPrint")
 const menuDark = document.querySelector(".menuDark")
 const saveCloseButton = document.querySelector(".saveCloseButton");
+const loadScreenBody = document.querySelector(".loadScreenBody");
+const loadScreenContainer = document.querySelector(".loadScreenContainer");
+const loadCloseButton = document.querySelector(".loadCloseButton");
+const clearMemButton = document.querySelector(".clearMemButton");
 saveScreenBody.style.display = "none";
 mainMenuBody.style.display = "none";
+loadScreenBody.style.display = "none";
 
+//initializing object to hold event data
+
+let currentEvent =  {
+    eventName: "event01",
+    unsortedParticipants: [],
+    teams:{
+        //team0000:[]
+    }
+};
+
+//initial list logic
+
+const newPoolParticipant = function (n) {
+    const newCont = document.createElement("div");
+    newCont.setAttribute("class","poolParticipantContainer");
+    const newName = document.createElement("div");
+    newCont.setAttribute("class","poolParticipant");
+    newName.innerText= n;
+    const newButton1 = document.createElement("button");
+    const newButton2 = document.createElement("button");
+    newButton1.setAttribute("class", "poolAddToTeamButton");    
+    newButton2.setAttribute("class", "poolDeleteButton"); 
+    newButton1.innerText = "+ Manual Add";
+    newButton2.innerText = "Delete";   
+    participantZone.append(newCont);
+    newCont.append(newName);
+    newCont.append(newButton1);
+    newCont.append(newButton2);
+}
+const addParticipant = function () {
+    currentEvent.unsortedParticipants.push({name: nameInput.value, tier: tierInput.value.toString().toUpperCase()})
+    newPoolParticipant(nameInput.value);
+    nameInput.value="";
+    tierInput.value="";
+}
+const resetParticipants = function (){
+    const listOfParticipants = document.querySelectorAll(".participantZone > div");
+    console.log(listOfParticipants);
+    for(i=0;i<listOfParticipants.length;i++){
+        listOfParticipants[i].style.display="none";
+    }
+    currentEvent.unsortedParticipants = [];
+    currentEvent.teams = {};
+}
+
+
+//opening and closing windows
 const toggleMenu = function (){
     if(mainMenuBody.style.display === "none"){
         mainMenuBody.style.display = "flex";
     }else{
         mainMenuBody.style.display = "none";
     }
+    
 }
 const toggleSave = function () {
     if(saveScreenBody.style.display === "none"){
@@ -56,7 +114,87 @@ const toggleSave = function () {
         saveScreenBody.style.display = "none";
     }
 }
+const toggleLoad = function () {
+    if(loadScreenBody.style.display === "none"){
+        loadScreenBody.style.display = "flex";
+        mainMenuBody.style.display = "none";
+    }else{
+        loadScreenBody.style.display = "none";
+    }
+}
+const clearLoadList = function (){
+    const listOfLoads = document.querySelectorAll(".loadItem");
+    for(i=0;i<listOfLoads.length;i++){
+        listOfLoads[i].style.display="none";
+    }
+}
+const menuOff = function (){
+    mainMenuBody.style.display = "none";
+}
+const loadOff = function (){
+    loadScreenBody.style.display = "none";
+}
+const saveOff = function (){
+    saveScreenBody.style.display = "none";
+}
 
-menuButton.onclick = toggleMenu;
-menuSave.onclick = toggleSave;
-saveCloseButton.onclick = toggleSave;
+//saving logic
+
+const saveData = function (){
+    
+    //take all data and put it into object
+    currentEvent.eventName = saveNameInput.value;
+    const savedData = Object.assign({},currentEvent)
+    console.log(savedData);
+    localStorage.setItem(currentEvent.eventName, savedData);
+    clearLoadList();
+    populateLoad();
+    saveOff();
+}
+const loadData = function (){
+    console.log(localStorage.getItem(e.target.innertext));
+    
+}
+const clearMemory = function (){
+    clearLoadList();
+    localStorage.clear()
+}
+
+const populateLoad = function () {
+    
+    for(let i=0; i<localStorage.length;i++){
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute("class","loadItem");
+        newDiv.innerText = Object.keys(localStorage)[i];
+        loadList.append(newDiv)
+    }     
+}
+populateLoad();
+clearMemButton.onclick = clearMemory;
+
+//on click events
+
+menuButton.onclick = function (){
+    toggleMenu();
+    saveOff();
+    loadOff();
+}
+menuSave.onclick = function(){
+    toggleSave();
+    loadOff();
+}
+menuLoad.onclick = function (){
+    menuOff();
+    saveOff();
+    toggleLoad();
+}
+saveCloseButton.onclick = saveOff;
+loadCloseButton.onclick = loadOff;
+saveButton.onclick = function () {
+    saveData();
+    saveOff();
+}
+enterPlayerButton.onclick = function (){
+    addParticipant();
+}
+resetPlayerButton.onclick = function(){resetParticipants()};
