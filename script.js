8541e478d9141adb1ec5ea56791fef9b450574f5
Pasha127 <pasha.account@gmail.com>
2022-07-31 23:06:41 +0200

    random assign works but not finished

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ let arrowIcons = document.querySelectorAll(".arrowIcon");
 let xIcons = document.querySelectorAll(".xIcon");
 let editTeamButtons = document.querySelectorAll(".editTeamButton");
 const teamInspectionContainer = document.querySelector(".teamInspectionContainer");
+const teamsContainer = document.querySelector(".teamsContainer");
 const teamInspectionTeamNumber = document.querySelector(".teamInspectionTeamNumber");
 const inspectionTeamNameInput = document.querySelector("#inspectionTeamNameInput");
 let teamInspectionItems = document.querySelectorAll(".teamInspectionItem");
@@ -72,7 +73,7 @@ let currentEvent =  {
 
 //initial list logic
 
-const newPoolParticipant = function (n,t) {
+const newPoolParticipant = function (n,t) {    
     const newCont = document.createElement("div");
     newCont.setAttribute("class","poolParticipantContainer");
     newCont.dataset.name = n;
@@ -105,20 +106,25 @@ const addParticipant = function () {
     const listOfParticipants = getUnsortedParticipants();
     let isEntered = false;
     for(participant of listOfParticipants){        
-        if(nameInput.value === participant.name){
+        if(nameInput.value.toLowerCase() === participant.name.toLowerCase()){
             isEntered = true;
             break;
         }
     }
-    if(nameInput.value !== "" && !isEntered){
+    if(nameInput.value !== "" && !isEntered && (tierInput.value.match(validNumbers) || tierInput.value === "" )){
         currentEvent.unsortedParticipants.push({name: nameInput.value, tier: tierInput.value.toString().toUpperCase()})
         newPoolParticipant(nameInput.value,tierInput.value);
         nameInput.value="";
         tierInput.value="";
         nameInput.style.backgroundColor = "#ffffff";
+        tierInput.style.backgroundColor = "#ffffff";
     }else{
-        nameInput.style.backgroundColor = "#ffaaaa";
+        if(nameInput.value === "" || isEntered){nameInput.style.backgroundColor = "#ffaaaa";};
+        if(!tierInput.value.match(validNumbers)){tierInput.style.backgroundColor = "#ffaaaa";};
+        
+        
     }
+    
 }
 const resetParticipants = function (){
     const listOfParticipants = document.querySelectorAll(".participantZone > div");
@@ -134,7 +140,7 @@ const resetParticipants = function (){
 const deletePFromUnSortList = function (target){
     const participant = target;
     const pName = participant.dataset.name;
-    console.log(pName);
+    //console.log(pName);
     for(i=0;i<currentEvent.unsortedParticipants.length;i++){
         if(currentEvent.unsortedParticipants[i].name === pName){currentEvent.unsortedParticipants.splice(i,1)}
     }
@@ -206,8 +212,8 @@ const clearMemory = function (){
 }
 const loadEvent = function(e){
     const newObj = JSON.parse(localStorage.getItem(e.target.innerText))
-    console.log(newObj);
-    console.log("loaded");
+    //console.log(newObj);
+    //console.log("loaded");
     resetParticipants();
 
     loadOff();
@@ -235,57 +241,168 @@ const getUnsortedParticipants = function (){
     for(participant of listOfParticipants){        
        // console.log(participant.dataset.name);
         outputList.push({name: participant.dataset.name, tier: participant.dataset.tier});
-
-    }
-
-    
+    }    
     return outputList;
 }
 
 const buildTeams = function (){
+    eraseTeams();
     const nOfTeams = teamNumberInput.value;
     if(teamNumberInput.value !== "" && teamNumberInput.value.match(validNumbers)){
         const listOfParticipants = getUnsortedParticipants();
         newTeams(nOfTeams, listOfParticipants);
-        console.log("teamSize:",nOfTeams);
+        //console.log("teamSize:",nOfTeams);
         teamNumberInput.style.backgroundColor = "#ffffff";
     }else{
         teamNumberInput.style.backgroundColor = "#ffaaaa";
     }
 }
-const newTeams = function (nOfTeams,listOfParticipants){
-    
+const eraseTeams = function (){
+    let teamCards = document.querySelectorAll(".teamCard");
+    for(i=0;i<teamCards.length;i++){
+        if(teamCards[i].dataset.locked !== "true"){
+            teamsContainer.removeChild(teamCards[i]);
+            
+        }else{            
+            for(names of teamCards){
+                if(names.querySelector(".cardTitle").innerText === teamCards[i].querySelector(".cardTitle").innerText){
+                    teamCards[i].querySelector(".cardTitle").innerText = `Saved Team ${i+1}`;
+                };
+            }
+        }
+    }
+};
+const newTeams = function (nOfTeams,listOfParticipants){    
+    for(i=0;i<nOfTeams;i++){
+        const newCont = document.createElement("div");
+        newCont.classList.add("teamCard")
+        teamsContainer.insertAdjacentHTML("beforeend",`<div class="teamCard" id="teamCardExample">
+        <div class="cardHeader" id="cardExampleHeader">
+            <div class="cardHeaderLine1" id="cardExampleHeaderLine1">
+                <div class="cardTitle" id="exampleCardTitle">Team ${i+1}</div>
+                <div class="lockIcon" id="exampleCardLock">
+                    <i class="fa-solid fa-lock invisible"></i>
+                    <i class="fa-solid fa-unlock"></i>
+                </div>                
+            </div>
+            <div class="cardHeaderLine2" id="cardExampleHeaderLine2">
+                <input class="teamNameInput" id="exampleCardTeamNameInput" placeholder="Team name">
+                <div class="cardTeamSize" id="exampleCardTeamSize">Team Size: <span></span></div>
+            </div>
+        </div>
+        <div class="cardTeamList" id="exampleCardTeamList">            
+        </div>`)
+        
 
+    }
+    
+    const cardList = teamsContainer.querySelectorAll(".cardTeamList");
+    for(i=0;i<listOfParticipants.length;i++){
+        const newEntry = document.createElement("div");
+        newEntry.classList.add("cardParticipant")
+        //console.log(cardList[i]);
+        cardList[Math.floor(Math.random() * nOfTeams)].append(newEntry);
+        const newPName = document.createElement("div");
+        //console.log(listOfParticipants[i]);
+        newPName.innerText=listOfParticipants[i].name;
+        newEntry.append(newPName);
+        newEntry.insertAdjacentHTML("beforeend",
+        `<div class="switchTeamButton" id="exampleCardSwitchTeamButton">
+        <div class="arrowIcon">=></div>
+        </div>
+        <div class="removeFromTeamButton" id="exampleCardRemoveFromTeamButton">
+            <div class="xIcon">X</div>
+        </div>`)
+
+    }
+    let newCards = teamsContainer.querySelectorAll(".teamCard")
+    for(i=0;i<newCards.length;i++){
+        newCards[i].querySelector(".cardTeamSize span").innerText = newCards[i].querySelectorAll(".cardParticipant").length;
+    }
+    let arrowIcons = document.querySelectorAll(".arrowIcon");
+    let xIcons = document.querySelectorAll(".xIcon");
+    let lockIcons = document.querySelectorAll(".lockIcon");
+    for(locks of lockIcons){locks.addEventListener("click",toggleLock)};
+    resetParticipants();
+    for(arrows of arrowIcons){arrows.addEventListener("click",switchTeams)};
+    resetParticipants();
+    for(xs of xIcons){xs.addEventListener("click", deleteFromTeam)};
+    resetParticipants();
+}
+const deleteFromTeam = function(){
+    console.log("send back to unordered list");
+}
+const switchTeams = function(){
+    console.log("switchTeams");
 }
 const clearSearchResult = function () {
     const listOfParticipants = document.querySelectorAll(".poolParticipant")
-    console.log(listOfParticipants);
+    //console.log(listOfParticipants);
     for(participant of listOfParticipants){
-        console.log(participant);
+        //console.log(participant);
         participant.style.border = "1px solid #000000";
     }
 }
 const searchForUP = function(){
+    let noMatch = true;
     if(searchInputUP.value !== ""){    
         const listOfParticipants = getUnsortedParticipants();
-        let noMatch = true;
         for(participant of listOfParticipants){
             //console.log(participant);
-            if(participant.name === searchInputUP.value){                
-                searchInputUP.style.backgroundColor = "#ffffff";
+            if(participant.name.toLowerCase() === searchInputUP.value.toLowerCase()){                
+                
                 const target = document.querySelector(`[data-name="${participant.name}"]`)
                 target.style.border = "6px solid #ffaaaa"
+                noMatch = false;
                 searchInputUP.value = "";
-                return;
+                
             }
-
-        }
-        
+        }        
     }
-    searchInputUP.style.backgroundColor = "#ffaaaa";
-    searchInputUP.value = "";
-    
+    if(!noMatch) {searchInputUP.style.backgroundColor = "#ffffff";}else{searchInputUP.style.backgroundColor = "#ffaaaa";};
+    searchInputUP.value = "";    
 }
+const lock = function (target){    
+    target.parentNode.querySelector(".fa-lock").classList.remove("invisible");
+    target.classList.add("invisible");
+    target.parentNode.parentNode.parentNode.parentNode.style.border = "2px solid #ffaaaa";
+    target.parentNode.parentNode.parentNode.parentNode.dataset.locked = "true";
+}
+const lockAll = function (){ 
+    let lockIcons = document.querySelectorAll(".lockIcon");   
+    for(locks of lockIcons){
+        locks.parentNode.querySelector(".fa-lock").classList.remove("invisible");        
+        locks.parentNode.parentNode.parentNode.style.border = "2px solid #ffaaaa";
+        locks.parentNode.querySelector(".fa-unlock").classList.add("invisible");
+        locks.parentNode.parentNode.parentNode.dataset.locked = "true";   
+    }
+}
+const unLockAll = function (){
+    let lockIcons = document.querySelectorAll(".lockIcon");    
+    for(locks of lockIcons){
+        locks.parentNode.querySelector(".fa-unlock").classList.remove("invisible");        
+        locks.parentNode.parentNode.parentNode.style.border = "1px solid #000000";
+        locks.parentNode.querySelector(".fa-lock").classList.add("invisible");   
+        locks.parentNode.parentNode.parentNode.dataset.locked = "false";   
+    }
+}
+const unlock = function (target){   
+    target.parentNode.parentNode.parentNode.parentNode.style.border = "1px solid #000000";
+    target.parentNode.querySelector(".fa-unlock").classList.remove("invisible");   
+    target.classList.add("invisible");
+    target.parentNode.parentNode.parentNode.parentNode.dataset.locked = "false";
+}
+const toggleLock = function (e){
+    const target = e.target;
+    if(target.classList.contains("fa-unlock"))
+    {lock(target);
+        //console.log("lock");
+    }else if(target.classList.contains("fa-lock")){ 
+        unlock(target)
+        //console.log("unlock");
+    };
+}
+
 
 //on click events
 
@@ -353,9 +470,24 @@ searchInputUP.addEventListener("keypress", function(e){
         searchForUP();
     }
 });
+lockTeamsButton.onclick = function (){
+    lockAll();
+}
+unlockTeamsButton.onclick = function (){
+    unLockAll();
+}
+resetTeamsButton.onclick = function (){
+    eraseTeams();
+}
+
+
+
 
 // reference:
 
 //removing key by value from local storage
 //Object.keys(localStorage).forEach((key) => {
     //    if (localStorage.getItem(key) === pName){ localStorage.removeItem(key);}})
+
+
+       
